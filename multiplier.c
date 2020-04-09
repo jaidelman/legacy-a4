d84c8eabaffb4b413b0922e87e6fa3cdaa610c32
jaidelman <jaidelma@uoguelph.ca>
2020-04-09 14:47:55 -0400

    Added timer

--- multiplier.c
+++ multiplier.c
@@ -6,7 +6,7 @@
   in C, both recursively and non recursively. */
 
 #include <stdio.h>
-
+#include <time.h>
 /* This function applies the algorithm resursively */
 int multiplierRecursive(int a, int b){
   if(a == 0){
@@ -44,6 +44,8 @@ int multiplier(int a, int b){
 /* Main function to run algorithm and get input */
 int main(){
   int a, b, c; //To store values
+  double time_spent; //To store time
+  clock_t begin, end; //For timing
 
   //Get input from user
   printf("Please enter your first number: ");
@@ -51,13 +53,19 @@ int main(){
   printf("Please enter your second number: ");
   scanf("%d", &b);
 
-  //Call recursive function and print result
+  //Call recursive function, time and print result
+  begin = clock();
   c = multiplierRecursive(a, b);
-  printf("Recursively:\n%d * %d = %d\n", a, b, c);
+  end = clock();
+  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Recursively:%d * %d = %d\nTime elapsed: %.10lf seconds\n", a, b, c, time_spent);
 
-  //Call non-recursive function and print result
-  c = multiplier(a, b);
-  printf("Non Recursively:\n%d * %d = %d\n", a, b, c);
+  //Call non-recursive function, time and print result
+  begin = clock();
+  c = multiplierRecursive(a, b);
+  end = clock();
+  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Non Recursively:%d * %d = %d\nTime elapsed: %.10lf seconds\n", a, b, c, time_spent);
 
   return 1;
 }
